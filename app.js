696bae20d80bead0dc2a7bf7a7ad62075b4fc3f2
tankg123 <ceo@amnhacso.com>
2026-01-06 23:41:40 +0700

    update chu hoa

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/qa", qaRoutes);
 
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
